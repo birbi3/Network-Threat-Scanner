cea42b96396df2343026220b467d7e409ea102ec
birbi3 <birbie@protonmail.com>
2019-06-19 20:54:31 -0500

    finished nmap scanner and parser

--- nmap_scan.py
+++ nmap_scan.py
@@ -1,13 +1,60 @@
 import os
+import json
+import socket
 
-#TODO get example nmap output file for prasing. 
-
-#Will run the nmap scan in the config file 
+#Will scan every port
 def heavy_scan(network):
 	with open("config", "r") as config: 
-		scan = config.readline()
-     os.system(scan + network + " > scan-results.txt")
-     with open("scan-results.txt", "r") as scan_results:
-         for stuff in scan_results:
+		#reads a nmap command form the config file. This might not be here long. 
+		scan = config.readline(0)
+     	os.system(scan + network + " > scan-results.txt")
+
+  
+def results_data(host,mac,port):
+	results = {"hosts": host, 
+	"mac": mac, 
+	"port": port
+		}
+	
+	return results
+
+def parse_scan():
+	heavy_scan()
+	with open('scan-results.txt', 'r') as results:
+		data = []
+		host = ""
+		mac = ""
+		flag = False
+		for line in results:
+
+			if "PORT" and "STATE"in line.strip():
+				flag = True
+				ports = []
+
+			elif "MAC" and "Address:" in line.strip():
+				mac = line.split(' ')[2]
+				host_info = results_data(host,mac,ports)
+				data.append(host_info)
+				host_info = None
+				
+				flag = False
+			
+			elif socket.gethostname() in line.strip():
+				break
+			
+			elif "Nmap scan report for " in line.strip():
+				host = line.split(' ')[4]
+
+			elif flag == True:
+				ports.append(line)
+				
+			
+	with open('results.json', 'w') as results:
+		results.write(json.dumps(data, indent=4, sort_keys=True))
+
+
+
+
+
 
 
